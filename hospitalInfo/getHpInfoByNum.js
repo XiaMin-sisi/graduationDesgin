0282f544a56bb5e63ebf1b125a95047a312c9256
xiamin <1532241857@qq.com>
2021-02-22 23:39:29 +0800

    医院信息相关接口

--- hospitalInfo/getHpInfoByNum.js
+++ hospitalInfo/getHpInfoByNum.js
@@ -2,36 +2,35 @@
 * 获取单个医院信息,通过医院账号
 * 参数：
 * userName *
-* passWord *
 */
 let {mysql,mysqlConf}= require('../utils/connectMysql');
 
 module.exports=(obj,callback)=>{
-    if(obj.method==="post"){
-        if(!obj.postParams.userName||!obj.postParams.passWord)
+    if(obj.method==="get"){
+        if(!obj.getParams.userNum)
         {
             callback(502,"必要参数缺失或不正确");
             return ;
         }
         let connect=mysql.createConnection(mysqlConf);
-        let sql=`select * from accountTb where accountNum = ${obj.postParams.userName} and accountPwd = ${obj.postParams.passWord}`;
+        let sql=`select * from hospitalMessTb where hospitalNum = ${obj.getParams.userNum}`;
         connect.query(sql,(error,res)=>{
             if(error)
             {
-                callback(200,{success:false,message:"deng",code:1003,data:error})
+                callback(200,{success:false,message:"",code:1003,data:error})
             }
             else {
                 if(res.length===0){
-                    callback(200,{success:true,message:"密码或账号错误！",code:1002,data:res[0]});
+                    callback(200,{success:true,message:"未找到该信息，请确认账号正确性！",code:1002,data:res[0]});
                     return ;
                 }
-                callback(200,{success:true,message:"登录成功",code:0,data:res[0]});
+                callback(200,{success:true,message:"ok",code:0,data:res[0]});
             }
         });
         connect.end();
     }
     else {
-        callback(501,"请求方式不正确，请使用Post请求！");
+        callback(501,"请求方式不正确，请使用Get请求！");
     }
 
 }
